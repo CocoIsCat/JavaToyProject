b105e05923c2a80ac4c0469cfb89ba7d84c62b6a
cocoiscat <demy176@naver.com>
2022-10-11 20:09:08 +0900

    fix: index 값 증가하지 않는 거 해결

--- src/Management/AddUser.java
+++ src/Management/AddUser.java
@@ -6,10 +6,10 @@ public class AddUser {
     public static void adduser(Customer[] newCustomers) {
         for (Customer CST : newCustomers) {
             try {
-                DataBase.customers[DataBase.index] = CST;
+                DataBase.customers[DataBase.index++] = CST;
             } catch (ArrayIndexOutOfBoundsException e) {
                 /*배열 크기 두 배로 늘리기*/
-                DataBase.customers[DataBase.index] = CST;
+                DataBase.customers[DataBase.index++] = CST;
             }
         }
     }
