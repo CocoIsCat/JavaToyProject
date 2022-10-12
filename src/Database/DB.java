666963b3c78e77a1f87b32d291df8267c64d630f
cocoiscat <demy176@naver.com>
2022-10-12 13:42:29 +0900

    refactor: customers 변경

--- src/Database/DB.java
+++ src/Database/DB.java
@@ -1,11 +1,12 @@
 package Database;
 
 import Customer.Customer;
+import Customer.Customers;
 import Group.GroupParameter;
 
 public class DB {
     public static int index = 0;
-    public static Customer[] customers = new Customer[10];
+    public static Customers customers = new Customers(new Customer[10]);
 
     public static GroupParameter[] groupParameters = new GroupParameter[3];
 }
