bdbf4e74dffd4157fe9e100eec5ad40b7a5231dc
cocoiscat <demy176@naver.com>
2022-10-14 13:09:43 +0900

    style: 출력 정렬

--- src/Menus/MainMenu.java
+++ src/Menus/MainMenu.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 
 public class MainMenu extends Menu{
     public static void printMainMenus() {
-        int menu = 0;
-        System.out.printf("[ %s ]\n","Main Menu");
-        System.out.println("========================================================================");
-        System.out.printf("|%-70s|\n"," 1. Manage Group");
-        System.out.printf("|%-70s|\n"," 2. Manege Customer");
-        System.out.printf("|%-70s|\n"," 3. Summary");
-        System.out.printf("|%-70s|\n"," 4. Quit");
-        System.out.println("========================================================================");
-        System.out.printf(" Choose One : ");
-        menu = new Scanner(System.in).nextInt();
-        System.out.println("\n");
-        chooseMainMenu(menu);
+        while(true) {
+            int menu = 0;
+            System.out.printf("[ %s ]\n", "Main Menu");
+            System.out.println("========================================================================");
+            System.out.printf("|%-70s|\n", " 1. Manage Group");
+            System.out.printf("|%-70s|\n", " 2. Manage Customer");
+            System.out.printf("|%-70s|\n", " 3. Summary");
+            System.out.printf("|%-70s|\n", " 4. Quit");
+            System.out.println("========================================================================");
+            System.out.printf(" Choose One : ");
+            menu = new Scanner(System.in).nextInt();
+            if (menu == 4) {
+                break;
+            }
+            else if(menu > 4 || menu < 1) {
+                System.out.println("\nfuck\n");
+            }
+            else {
+                System.out.println("\n");
+                chooseMainMenu(menu);
+            }
+        }
     }
 
     private static void chooseMainMenu(int menu) {
@@ -29,10 +39,7 @@ public class MainMenu extends Menu{
             case 3:
                 SummaryMenu.printSummaryMenus();
                 break;
-            case 4:
-                break;
-            default:
-                printMainMenus();
         }
     }
 }
+
